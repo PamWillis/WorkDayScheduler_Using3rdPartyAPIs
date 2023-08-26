97f543586cbdd3bc02dcf2b4f8e58c8fc57a718e
Pam <pwillisop@yahoo.com>
2023-08-26 13:10:08 -0400

    final completion

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ The work day schedular is used to save a schedule of your 8hr day. Hours are den
 
 ## Usage
 
-The date and time can be found across the top this is synced with day.js and is linked to the id #currentDay located in the header. to show your local date and time. You are welcome to enter items in the red or green bars. These denote present and future times. Click to save your item with the button on the right.
+The date and time can be found across the top this is synced with day.js and is linked to the id #currentDay located in the header to show your local date and time. You are welcome to enter items in the bars. These denote past, present and future times. Click to save your item with the button on the right. When it is saved a message will pop up for 5 seconds saying it has been saved to local storage. When the page is refreshed, information from the local storage will be placed in page again using the getLocalStorage method using a for loop to replace each hour. The colors of the bar are updated to check that the past present and future are still correct.
+
+![MockUp](./assets/img/screenShot.png)
 
 ## Deployment and GitHub
  deployment: https://pamwillis.github.io/WorkDayScheduler_Using3rdPartyAPIs/
@@ -38,4 +40,4 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE..
 
 ## Credit
-This was created with assistants from AskBCS Learning assistants and what was demonstrated in class.
+This was created with assistants from AskBCS Learning assistants, my tutor Juan Delgato for better explaining localStorage, my classmate Zak Richardson for typo checking and reviewing hide and show, and what was demonstrated in class.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
 
 $(function () { //wrapped code
-  // localStorage.getItem(time, value);
-
-
-  console.log("hello");
-
   // date and time to top of page
   var today = dayjs();
   $('lead').text(today.format('MMM D, YYYY'));
@@ -12,17 +7,16 @@ $(function () { //wrapped code
 
   $('lead').text(reformatDate);
   document.getElementById("currentDay").innerHTML = reformatDate;
-  console.log(reformatDate)
+  //console.log(reformatDate)
 
   function hourUpdater() {
     var currentHour = dayjs().hour();
     // line 20 to adjust time to test
-    currentHour = currentHour - 10;
-    console.log("this " + currentHour)
+    // currentHour = currentHour - 10;
+   // console.log("this " + currentHour)
     $('.time-block').each(function () {
       var blockHour = parseInt($(this).attr('id').split('-')[1]);
       // check for past present or future add to class
-
       if (blockHour < currentHour) {
         $(this).addClass('past');
 
@@ -40,16 +34,14 @@ $(function () { //wrapped code
   };
 
   //get info from entry and save to Local Storage
-  // const saveData = () => {
   $('.saveBtn').on('click', function () {
     //the time of the div class
     let time = $(this).parent().attr('id');
     //get the value of the text
     let value = $(this).siblings('.description').val();
-
     //save in local storage and saves session for refresh
-    localStorage.setItem("time", "value");
-    console.log(value);
+    localStorage.setItem(time, value);
+    //console.log(value);
     notification();
   });
 
@@ -67,5 +59,56 @@ $(function () { //wrapped code
   hourUpdater();
 
   setInterval(hourUpdater, 15000);
+
+  // let valueforNine = localStorage.getItem("hour-9")
+  // console.log(vallueforNine)
+  // let timeblockNine = $("#hour-9").children("textarea")
+  // console.log(timeblockNine)
+  // $("#hour-9").children("textarea").val(valueforNine)
+  // $("#hour-10").children("textarea").val(localStorage.getItem("hour-10"))
+  // $("#hour-11").children("textarea").val(localStorage.getItem("hour-11"))
+  // $("#hour-12").children("textarea").val(localStorage.getItem("hour-12"))
+// resetting information upon refresh
+  for(var i = 9; i < 18; i++){
+    $("#hour-"+i).children("textarea").val(localStorage.getItem("hour-"+i))
+  }
+
 });
 
+
+//tutor explaining localStorage
+
+// How to save and Item and get an item using locat storage
+// let fruit = "banana";
+// let Keyname = "myfruits"
+
+// localStorage.setItem(Keyname,fruit)
+
+// let myfruit = localStorage.getItem("favFruits")
+
+// console.log("My favorite Fruit is: " + myfruit)
+
+// ------
+//saving javascript items into localstorage
+
+// let myFruits = ["apple","strawberry", "watermelon"];
+
+// localStorage.setItem("listOfFruits", JSON.stringify(myFruits))
+
+// let getBackMyLlst = JSON.parse( localStorage.getItem("listOfFruits"))
+
+// console.log("This is my list: " + getBackMyLlst)
+// console.log(getBackMyLlst[0])
+
+
+// let favNumber = {
+//   number: 5,
+//   name: "juan"
+// }
+
+
+// localStorage.setItem("number", JSON.stringify( favNumber) )
+
+// let getbacknumber = JSON.parse( localStorage.getItem("number"))
+// console.log("this is number: " + getbacknumber)
+// console.log(getbacknumber.number)
